82cadc0fa36747febc1e7e7b6b4e12e7923631bb
hackedlings <pks9862728888@gmail.com>
2020-11-10 01:14:54 +0530

    .

--- FrontendApp/src/environments/environment.prod.ts
+++ FrontendApp/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://buybook.infinityfreeapp.com/',
+  db_name: 'epiz_27154840_buybook_db',
+  db_user_name: 'epiz_27154840',
+  db_host_name: 'sql306.epizy.com',
+  db_password: 'IKXCC5NddFz',
+  db_port: 3306
 };
